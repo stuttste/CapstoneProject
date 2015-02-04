cd842588a726ee225c73124cf45f3d8a2941c041
stuttste <e_stutts@hotmail.com>
2015-02-04 15:02:09 -0600

    Restructured JavaScript File Locations and Created Login and Registration Pages/Files

---
+++ includes/psl-config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "localhost");     // The host you want to connect to.
+define("USER", "pet_user");    // The database username. 
+define("PASSWORD", "asUGVWD8FZRnW9uB");    // The database password. 
+define("DATABASE", "MainDB");    // The database name.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+?>
