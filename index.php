5fcf737c65d5075677f6aa561a81443369e84404
Victor <vicdsp1532@gmail.com>
2015-02-22 20:11:30 -0600

    Victor,results

--- index.php
+++ index.php
@@ -24,11 +24,7 @@ sec_session_start();
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
 	<link rel="stylesheet" type="text/css" href="css/global.css">
-	<link rel="stylesheet" href="http://cdn.datatables.net/1.10.2/css/jquery.dataTables.min.css"></style>
-	<script type="text/javascript" src="http://cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"></script>
-	<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	-->
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 	</head>
 
@@ -144,84 +140,67 @@ sec_session_start();
 									</thead>  
 									<tbody>  
 										<tr>  
-											<td>001</td>  
-											<td>Anusha</td>  
-											<td>India</td>  
-											<td>10000</td>  
+											<td></td>  
+											<td></td>  
+											<td></td>  
+											<td></td>  
 										</tr>  
 										<tr>  
-											<td>002</td>  
-											<td>Charles</td>  
-											<td>United Kingdom</td>  
-											<td>28000</td>  
+											<td></td>  
+											<td></td>  
+											<td></td>  
+											<td></td>  
 										</tr>  
 										<tr>  
-											<td>003</td>  
-											<td>Sravani</td>  
-											<td>Australia</td>  
-											<td>7000</td>  
+											<td></td>  
+											<td></td>  
+											<td></td>  
+											<td></td>  
 										</tr>  
 										<tr>  
-											<td>004</td>  
-											<td>Amar</td>  
-											<td>India</td>  
-											<td>18000</td>  
+											<td></td>  
+											<td></td>  
+											<td></td>  
+											<td></td>  
 										</tr>  
 										<tr>  
-											<td>005</td>  
-											<td>Lakshmi</td>  
-											<td>India</td>  
-											<td>12000</td>  
+											<td></td>  
+											<td></td>  
+											<td></td>  
+											<td></td>  
 										</tr>  
 										<tr>  
-											<td>006</td>  
-											<td>James</td>  
-											<td>Canada</td>  
-											<td>50000</td>  
+											<td></td>  
+											<td></td>  
+											<td></td>  
+											<td></td>  
 										</tr>  
           								<tr>  
-											<td>007</td>  
-											<td>Ronald</td>  
-											<td>US</td>  
-											<td>75000</td>  
+											<td></td>  
+											<td></td>  
+											<td></td>  
+											<td></td>  
 										</tr>  
 										<tr>  
-											<td>008</td>  
-											<td>Mike</td>  
-											<td>Belgium</td>  
-											<td>100000</td>  
+											<td></td>  
+											<td></td>  
+											<td></td>  
+											<td></td>  
 										</tr>  
 										<tr>  
-											<td>009</td>  
-											<td>Andrew</td>  
-											<td>Argentina</td>  
-											<td>45000</td>  
+											<td></td>  
+											<td></td>  
+											<td></td>  
+											<td></td>  
 										</tr>  
 										<tr>  
-											<td>010</td>  
-											<td>Stephen</td>  
-											<td>Austria</td>  
-											<td>30000</td>  
-										</tr>  
-										<tr>  
-											<td>011</td>  
-											<td>Sara</td>  
-											<td>China</td>  
-											<td>750000</td>  
-										</tr>  
-										<tr>  
-											<td>012</td>  
-											<td>JonRoot</td>  
-											<td>Argentina</td>  
-											<td>65000</td>  
+											<td></td>  
+											<td></td>  
+											<td></td>  
+											<td></td>  
 										</tr>  
 										</tbody>  
 								</table>  
-								<script>
-									$(document).ready(function(){
-									$('#myTable').dataTable();
-									});
-								</script>
 							</div>
 					</div>
 				</div>
