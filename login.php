062cd037deb9b29b615d1f77ae51d2f1c9710915
stuttste <e_stutts@hotmail.com>
2015-03-17 09:08:53 -0500

    Revised Login Page

--- login.php
+++ login.php
@@ -1,5 +1,7 @@
+<!DOCTYPE html>
 <?php
 include_once 'includes/db_connect.php';
+include_once 'includes/register.inc.php';
 include_once 'includes/functions.php';
  
 sec_session_start();
@@ -10,47 +12,106 @@ if (login_check($mysqli) == true) {
     $logged = 'out';
 }
 ?>
-<!DOCTYPE html>
 <html>
     <head>
         <title>Secure Login: Log In</title>
-        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.2-dist/css/bootstrap.css">
-		<link rel="stylesheet" type="text/css" href="css/global.css">
-		<link rel="stylesheet" type="text/css" href="css/signin.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+		<link rel="stylesheet" href="css/global.css">
         <script type="text/JavaScript" src="js/sha512.js"></script> 
         <script type="text/JavaScript" src="js/forms.js"></script> 
     </head>
     <body>
-        <?php
+		<?php
         if (isset($_GET['error'])) {
             echo '<p class="error">Error Logging In!</p>';
         }
         ?> 
 		<div class="container">
-			<div class="jumbotron">
-				<form class="form-signin" action="includes/process_login.php" method="post" name="login_form"> 
-					<h2 class="form-signin-heading">Please sign in</h2>
-					<label for="inputEmail" class="sr-only">Email Address</label>
-					<input type="email" name="email" class="form-control" placeholder="Email address" />
-					<label for="inputPassword" class="sr-only">Password</label>
-					<input type="password" name="password" id="password" class="form-control" placeholder="Password"/>
+			<div class = "row-fluid">
+				<div class = "col-md-12" style="text-align: center">
+					<h1>Welcome to the PET Tool!</h1>
+				</div>
+				</div>
+			<div class="row-fluid vertical-center" >
+				<div class="col-md-5">
+					<form class="form-signin" action="includes/process_login.php" method="post" name="login_form"> 
+						<h2 class="form-signin-heading">Sign In</h2>
+						<label for="inputEmail" class="sr-only">Email Address</label>
+						<input type="email" name="email" class="form-control" placeholder="Email address" />
+						<label for="inputPassword" class="sr-only">Password</label>
+						<input type="password" name="password" id="password" class="form-control" placeholder="Password"/>
+						<input type="button" 
+							   value="Login" 
+							   onclick="formhash(this.form, this.form.password);"
+							   class="btn btn-lg btn-primary btn-block" /> 
+					</form>
+					<?php
+							if (login_check($mysqli) == true) {
+											echo '<h6>Currently logged ' . $logged . ' as ' . htmlentities($_SESSION['username']) . '.</h6>';
+					 
+								echo '<h6>Do you want to change user? <a href="includes/logout.php">Log out</a>.</h6>';
+							} else {
+											echo '<h6>Currently logged ' . $logged . '.</h6>';
+											echo "<h6>If you don't have a login, please <a href='register.php'>register</a></h6>";
+									}
+					?>   
+				</div>
+				
+        <!-- Registration form to be output if the POST variables are not
+        set or if the registration script caused an error. -->
+        <div class="col-md-5 col-md-offset-2">
+				<h1>Make an Account</h1>
+				<?php
+					if (!empty($error_msg)) {
+						echo $error_msg;
+					}
+					?>
+        <ul class="list-group">
+					<li>Usernames may contain only digits, upper and lower case letters and underscores</li>
+					<li>Emails must have a valid email format</li>
+					<li>Passwords must be at least 6 characters long</li>
+					<li >Passwords must contain
+						<ul>
+							<li>At least one upper case letter (A..Z)</li>
+							<li>At least one lower case letter (a..z)</li>
+							<li>At least one number (0..9)</li>
+						</ul>
+					</li>
+					<li>Your password and confirmation must match exactly</li>
+				</ul>
+				<form action="<?php echo esc_url($_SERVER['PHP_SELF']); ?>" 
+						method="post" 
+						name="registration_form"
+						class="form-signin">
+						<label for="inputUsername" class="sr-only">Username</label>
+						<input type='text' 
+							name='username' 
+							id='username' class="form-control" placeholder="Username"/>
+						<label for="inputEmail" class="sr-only">Email</label>
+						<input type="text" name="email" id="email" class="form-control" placeholder="Email" />
+						<label for="inputPassword" class="sr-only">Password</label>
+						<input type="password"
+									 name="password" 
+									 id="password" class="form-control" placeholder="Password" />
+						<label for="inputPassword" class="sr-only">Password</label>
+						<input type="password" 
+											 name="confirmpwd" 
+											 id="confirmpwd" class="form-control" placeholder="Retype Password" />
 					<input type="button" 
-						   value="Login" 
-						   onclick="formhash(this.form, this.form.password);"
-						   class="btn btn-lg btn-primary btn-block" /> 
+						   value="Register" 
+						   onclick="return regformhash(this.form,
+										   this.form.username,
+										   this.form.email,
+										   this.form.password,
+										   this.form.confirmpwd);" 
+							class="btn btn-lg btn-primary btn-block" /> 
 				</form>
- 
-<?php
-        if (login_check($mysqli) == true) {
-                        echo '<h6>Currently logged ' . $logged . ' as ' . htmlentities($_SESSION['username']) . '.</h6>';
- 
-            echo '<h6>Do you want to change user? <a href="includes/logout.php">Log out</a>.</h6>';
-        } else {
-                        echo '<h6>Currently logged ' . $logged . '.</h6>';
-                        echo "<h6>If you don't have a login, please <a href='register.php'>register</a></h6>";
-                }
-?>      
-			</div>
+				
 		</div>
     </body>
 </html>
+
+
+
+
