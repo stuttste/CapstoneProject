c05dace3d08919e9f395bfc21cacfb96a61fcc32
stuttste <e_stutts@hotmail.com>
2015-03-17 23:12:15 -0500

    Attempt 4?

---
+++ verify.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<?php
+include_once 'includes/db_connect.php';
+include_once 'includes/register.inc.php';
+include_once 'includes/functions.php';
+?>
+<html>
+    <head>
+        <title>Verify Account</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+		<link rel="stylesheet" href="css/global.css">
+        <script type="text/JavaScript" src="js/sha512.js"></script> 
+        <script type="text/JavaScript" src="js/forms.js"></script> 
+		<script type="text/JavaScript">
+			if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])){
+				// Verify data
+				$email = mysql_escape_string($_GET['email']); // Set email variable
+				$hash = mysql_escape_string($_GET['hash']); // Set hash variable
+				
+				$search = mysql_query("SELECT email, hash, active FROM MEMBERS WHERE email='".$email."' AND hash='".$hash."' AND active='0'") or die(mysql_error()); 
+				$match  = mysql_num_rows($search);
+				alert($match);
+			}else{
+				// Invalid approach
+			}
+		</script>
+    </head>
+    <body>
+	
+    </body>
+</html>
+
+
+
+
