b1f902378cd595404a75eb2b61a65cacbdd0997b
stuttste <e_stutts@hotmail.com>
2015-03-18 10:02:49 -0500

    Refinement 3

--- verify.php
+++ verify.php
@@ -18,7 +18,7 @@ include_once 'includes/functions.php';
 		<script type= "text/JavaScript">
 			$(document).ready(function(){
 				var flag = $('value').get('text');
-				if($flag){
+				if(flag){
 					setInterval(function(){window.location.href = "http://g3cap.tk/staging/login.php"}, 5000);
 				}
 			});
