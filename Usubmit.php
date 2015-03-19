b7845c0f9c68d082a12f95ef6b3a15ffadab6b51
Torrx43j2 <Genericemail9000@gmail.com>
2015-03-19 13:05:25 -0500

    Another php test
    This one should do something!

--- Usubmit.php
+++ Usubmit.php
@@ -4,31 +4,22 @@ include 'includes/functions.php';
  
 sec_session_start();
 
- if (login_check($mysqli) == false) {
+ if (login_check($mysqli) == false) 
+ {
 	header('Location: login.php');
 	die();
-}
-
-
-if( isset($_POST['submit']) )
+  }
+if (isset ($_POST['submit'])
 {
-    
-    $phage = htmlentities($_POST['phagec']);
+	$phage = htmlentities($_POST['phagec']);
 	$cluster = htmlentities($_POST['clustc']);
 	$subcluster = htmlentities($_POST['subc']);
 	$enzyme= htmlentities($_POST['enzymec']);
 	$cuts = htmlentities($_POST['cutc']);
     $username= 'test';
-    $sql =	 "INSERT INTO SUBMISSIONS (INSERT INTO `MainDB`.`SUBMISSIONS` (`Name`, `Cluster`, `Subcluster`, `Enzyme`, `Cuts`, `Email`) VALUES ('$phage','$cluster','$subcluster','$enzyme','$cut','$username'));
+    $sql ="INSERT INTO SUBMISSIONS (INSERT INTO `MainDB`.`SUBMISSIONS` (`Name`, `Cluster`, `Subcluster`, `Enzyme`, `Cuts`, `Email`) VALUES ('$phage','$cluster','$subcluster','$enzyme','$cut','$username'));""
 
 mysqli_query($mysqli, $sql);
-echo 'your request is pending';
+echo "your request is pending"";
 }
-
-
-	
-
-
-
-
-?>
+?> 
