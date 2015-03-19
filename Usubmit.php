e1d686749d23ca8cd3c8b9d10d17bb88f6aa7b09
Torrx43j2 <Genericemail9000@gmail.com>
2015-03-19 13:10:39 -0500

    Fixed quotes
    ...

--- Usubmit.php
+++ Usubmit.php
@@ -17,9 +17,9 @@ if (isset ($_POST['submit'])
 	$enzyme= htmlentities($_POST['enzymec']);
 	$cuts = htmlentities($_POST['cutc']);
     $username= 'test';
-    $sql ="INSERT INTO SUBMISSIONS (INSERT INTO `MainDB`.`SUBMISSIONS` (`Name`, `Cluster`, `Subcluster`, `Enzyme`, `Cuts`, `Email`) VALUES ('$phage','$cluster','$subcluster','$enzyme','$cut','$username'));""
+    $sql ="INSERT INTO SUBMISSIONS (INSERT INTO `MainDB`.`SUBMISSIONS` (`Name`, `Cluster`, `Subcluster`, `Enzyme`, `Cuts`, `Email`) VALUES ('$phage','$cluster','$subcluster','$enzyme','$cut','$username'));"
 
 mysqli_query($mysqli, $sql);
-echo "your request is pending"";
+echo "your request is pending";
 }
 ?> 
