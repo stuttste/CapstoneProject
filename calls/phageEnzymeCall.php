1e255f0e416c759c68554d751394fedb537a8b74
stuttste <e_stutts@hotmail.com>
2015-03-18 20:57:20 -0500

    2002

--- calls/phageEnzymeCall.php
+++ calls/phageEnzymeCall.php
@@ -4,7 +4,6 @@
 	
 	$type = $_GET['type'];
 	$id = $_GET['id'];
-	$dom = new DOMDocument('1.0', 'utf-8');
 	
 	if($type='Phage'){
 		$sql=$mysqli->query("SELECT `Name`, `Cluster`, `Subcluster` FROM `PHAGE` WHERE `Name`= '".$id."'");
@@ -18,7 +17,8 @@
 	
 	if($type='Phage' || $type='Cluster' || $type='Subcluster'){
 		while($row = $sql->fetch_assoc()){
-			echo '<tr><td>'. $row["Name"] .'</td><td>'. $row["Cluster"] .'</td><td>'. $row["Subcluster"] .'</td></tr>';
+			echo $row["Name"] .', '. $row["Cluster"] .', '. $row["Subcluster"] ;
+			//echo '<tr><td>'. $row["Name"] .'</td><td>'. $row["Cluster"] .'</td><td>'. $row["Subcluster"] .'</td></tr>';
 		}
 	}elseif($type='Enzyme'){
 		while($row = $sql->fetch_assoc()){
