df4d6c562630d6f074ae6ded20e6f38bd4010f94
Torrx43j2 <Genericemail9000@gmail.com>
2015-03-18 21:23:00 -0500

    Testing yet again

--- Usubmit.php
+++ Usubmit.php
@@ -1,8 +1,14 @@
-<?
-php
-
-
+<?php
+include 'includes/db_connect.php';
+include 'includes/functions.php';
+ 
 sec_session_start();
+
+ if (login_check($mysqli) == false) {
+	header('Location: login.php');
+	die();
+}
+
 
 if( isset($_POST['submit']) )
 {
