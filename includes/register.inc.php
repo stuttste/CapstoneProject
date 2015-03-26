c8b4a2e4f22d5bc37cbb07db720b2b64644e98ef
Torrx43j2 <Genericemail9000@gmail.com>
2015-03-25 22:57:26 -0500

    pt 3

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -9,14 +9,15 @@ if (isset($_POST['username'], $_POST['email'], $_POST['p'], $POST['fname'], $POS
     $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
     $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
     $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+		  $Fname = filter_input(INPUT_POST, 'Fname', FILTER_SANITIZE_STRING);
+	    $Lname = filter_input(INPUT_POST, 'Lname', FILTER_SANITIZE_STRING);
+		  $State = filter_input(INPUT_POST, 'State', FILTER_SANITIZE_STRING);
+		    $Univ = filter_input(INPUT_POST, 'Univ', FILTER_SANITIZE_STRING);
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         // Not a valid email
         $error_msg .= '<p class="error">The email address you entered is not valid</p>';
     }
-	  $Fname = filter_input(INPUT_POST, 'Fname', FILTER_SANITIZE_STRING);
-	    $Lname = filter_input(INPUT_POST, 'Lname', FILTER_SANITIZE_STRING);
-		  $State = filter_input(INPUT_POST, 'State', FILTER_SANITIZE_STRING);
-		    $Univ = filter_input(INPUT_POST, 'Univ', FILTER_SANITIZE_STRING);
+
     $password = filter_input(INPUT_POST, 'p', FILTER_SANITIZE_STRING);
     if (strlen($password) != 128) {
         // The hashed pwd should be 128 characters long.
@@ -85,7 +86,7 @@ if (isset($_POST['username'], $_POST['email'], $_POST['p'], $POST['fname'], $POS
  
         // Insert the new user into the database 
         if ($insert_stmt = $mysqli->prepare("INSERT INTO MEMBERS (Username, Email, Password, Salt,Fname,Lname,State,University) VALUES (?, ?, ?, ?, ?, ?, ?, ?)")) {
-            $insert_stmt->bind_param('ssss', $username, $email, $password, $random_salt,$Fname,$Lname,$State,$Univ);
+            $insert_stmt->bind_param( $username, $email, $password, $random_salt,$Fname,$Lname,$State,$Univ);
             // Execute the prepared query.
             if (! $insert_stmt->execute()) {
                 header('Location: ../error.php?err=Registration failure: INSERT');
