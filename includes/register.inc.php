3a8492c63ede2fa26adb5a2c35f3e288db4fa1b3
Torrx43j2 <Genericemail9000@gmail.com>
2015-03-27 15:13:40 -0500

    Part 5

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -4,7 +4,7 @@ include_once 'psl-config.php';
  
 $error_msg = "";
  
-if (isset($_POST['username'], $_POST['email'], $_POST['p'], $POST['Fname'], $POST ['Lname'] $POST ['State'], $POST['Univ'])) {
+if (isset($_POST['username'], $_POST['email'], $_POST['p'], $_POST['Fname'], $_POST ['Lname'] $_POST ['State'], $_POST['Univ'])) {
     // Sanitize and validate the data passed in
     $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
     $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
