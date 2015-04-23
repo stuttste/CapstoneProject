f7c5e99ea035f04988d095a3ccb422f2981813cd
stuttste <e_stutts@hotmail.com>
2015-04-22 21:06:48 -0500

    Phage Searching 3.17 - Forgot the closing button tag

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ sec_session_start();
 			for(var i = 0; i < enzArr.length; i++){
 				htmlOut += '<div class="input-group col-md-3"><span class="input-group-addon">' + enzArr[i] + '</span>';
 				htmlOut += '<button class="btn btn-default btn-xs dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false" id="' + enzArr[i] + '">';
-				htmlOut += '<ul class="dropdown-menu" role="menu"><li>None</li><li>Few</li><li>Normal</li><li>Many</li><li>Lots</li></ul>';
+				htmlOut += '<ul class="dropdown-menu" role="menu"><li>None</li><li>Few</li><li>Normal</li><li>Many</li><li>Lots</li></ul></button>';
 				htmlOut += '</span></div><br />';
 			}
 			
