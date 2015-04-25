49c569f5b7a0ee2be1e7693dabd6fdb9d4b182df
schutzvl <vicdsp1532@gmail.com>
2015-04-25 18:46:11 -0500

    Victor, remove email attempt

--- addInsertDelete.php
+++ addInsertDelete.php
@@ -1,7 +1,7 @@
 <?php
 	$email = $_POST['email'];
 	function deleteRow(){
-		if ($sql = $mysqli->prepare("DELETE FROM `MEMBERS` WHERE 'MEMBERS`.`Email` = "$email"")) {
+		if ($sql = $mysqli->prepare("DELETE FROM `MEMBERS` WHERE 'MEMBERS`.`Email` = '$email'")) {
 			$sql->execute();
 		}
 			$sql->close();
