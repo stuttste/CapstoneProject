0cc0463412aca91112fa98b7e04c3aa90e16cb72
schutzvl <vicdsp1532@gmail.com>
2015-04-25 12:22:16 -0500

    Victor, admintools delete email

--- AdminToolsPage.php
+++ AdminToolsPage.php
@@ -13,7 +13,7 @@ sec_session_start();
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <title>G3Capstone Staging</title>
+    
 	
 	<!--<link rel="stylesheet" type="text/css" 
 
