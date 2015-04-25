b8dca548e8a91dd18b2a8aea9869332ab570fb7b
stuttste <e_stutts@hotmail.com>
2015-04-25 18:10:21 -0500

    Right align 5

--- index.php
+++ index.php
@@ -220,7 +220,7 @@ sec_session_start();
 						<li ><a href="demographics.html">Demographics<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-user"></span></a></li>
 						</ul>
 						<div class="nav navbar-nav pull-right">
-							<p class="navbar-text"><a href="includes/logout.php">Log Out<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-log-out"></span></a></p>
+							<a href="includes/logout.php">Log Out<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-log-out"></span></a>
 						</div>
 				</div>
 			</div>
