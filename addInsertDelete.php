7e5c3d597a20f986df7c900e4613bb0a7b01b9cd
schutzvl <vicdsp1532@gmail.com>
2015-04-25 21:24:36 -0500

    Victor, remove email attempt

--- addInsertDelete.php
+++ addInsertDelete.php
@@ -1,5 +1,13 @@
 <?php
+include 'includes/db_connect.php';
+include 'includes/functions.php';
+ 
+sec_session_start();
 
+ if (login_check($mysqli) == false) {
+	header('Location: login.php');
+	die();
+}
 
 	$email = $_POST['email'];
 	//deleteRow($email);
