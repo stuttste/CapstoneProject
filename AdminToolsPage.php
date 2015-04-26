34408ddb0dabb737c60deef6d4d2d936b92e6aa6
schutzvl <vicdsp1532@gmail.com>
2015-04-25 22:03:51 -0500

    Victor, remove email attempt

--- AdminToolsPage.php
+++ AdminToolsPage.php
@@ -120,7 +120,7 @@ href="jQuery/jquery-1.11.2.min.js"></script>
 									</thead>
 									<tbody>
 										<?php
-											if($sql = $mysqli->prepare("SELECT `Username`, `Email`, `Admin` FROM `MEMBERS`")){													
+											if($sql = $mysqli->prepare("SELECT `Username`, `Email`, `Admin` FROM `MEMBERS` WHERE Email = 'schutzvl@warhawks.ulm.edu'")){													
 												$sql->execute();
 													$sql->bind_result($username, $email, $admin);
 													while($sql->fetch()){
