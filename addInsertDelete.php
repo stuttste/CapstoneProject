4dbee163439beda24644a4acf4e3eed890d7f41b
schutzvl <vicdsp1532@gmail.com>
2015-04-25 22:58:03 -0500

    Victor, remove email attempt

--- addInsertDelete.php
+++ addInsertDelete.php
@@ -11,10 +11,10 @@ sec_session_start();
 
 	//$email = $_POST['email'];
 	$email = "schutzvl@warhawks.ulm.edu";
-	deleteRow($email);
+	//deleteRow($email);
 	function deleteRow(){
-		if ($sql = $mysqli->prepare("DELETE FROM `MEMBERS` WHERE 'MEMBERS`.`Email` = 'schutzvl@warhawks.ulm.edu'" )) {
-			//$sql->bind_param("s",$email);
+		if ($sql = $mysqli->prepare("DELETE FROM `MEMBERS` WHERE 'MEMBERS`.`Email` = 's'" )) {
+			$sql->bind_param('s',$email);
 			$sql->execute();
 		}
 			$sql->close();
