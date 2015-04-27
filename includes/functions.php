91816bbfa31b72cdbc7ca2a22f90e5bd388a157c
stuttste <e_stutts@hotmail.com>
2015-04-27 14:44:51 -0500

    Placed restrictions on admin tools. Only admins should be able to see the link on the index page.

--- includes/functions.php
+++ includes/functions.php
@@ -162,7 +162,50 @@ function login_check($mysqli) {
     }
 }
 
-
+function admin_check($mysqli) {
+    // Check if all session variables are set 
+    if (isset($_SESSION['user_id'], 
+                        $_SESSION['username'], 
+                        $_SESSION['login_string'])) {
+ 
+        $user_id = $_SESSION['user_id'];
+        $login_string = $_SESSION['login_string'];
+        $username = $_SESSION['username'];
+ 
+        // Get the user-agent string of the user.
+        $user_browser = $_SERVER['HTTP_USER_AGENT'];
+ 
+        if ($stmt = $mysqli->prepare("SELECT Admin
+                                      FROM MEMBERS
+                                      WHERE ID = ? LIMIT 1")) {
+            // Bind "$user_id" to parameter. 
+            $stmt->bind_param('i', $user_id);
+            $stmt->execute();   // Execute the prepared query.
+            $stmt->store_result();
+ 
+            if ($stmt->num_rows == 1) {
+                // If the user exists get variables from result.
+                $stmt->bind_result($admin);
+                $stmt->fetch();
+				if($admin == 1){
+					return true;
+                } else {
+                    // Not admin
+                    return false;
+                }
+            } else {
+                // Not logged in 
+                return false;
+            }
+        } else {
+            // Not logged in 
+            return false;
+        }
+    } else {
+        // Not logged in 
+        return false;
+    }
+}
 
 function esc_url($url) {
  
