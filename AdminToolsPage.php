babb2049bbf0987e1e7cf3fba6a8d20757aa4c2d
schutzvl <vicdsp1532@gmail.com>
2015-04-28 00:04:32 -0500

    Victor, admin page

--- AdminToolsPage.php
+++ AdminToolsPage.php
@@ -204,7 +204,14 @@ href="jQuery/jquery-1.11.2.min.js"></script>
 													$sql->bind_result($username, $email, $admin);
 													while($sql->fetch()){
 															echo '<tr class= "'.$username.'"><td>'.$username.'</td><td class="email">'.$email.'</td><td>'.$admin.'</td></tr>';
-													}															}
+																if($admin == 1){
+																	$admin = 'yes';
+																}
+																else if($admin == 0){
+																	$admin = 'no';
+																}
+													}															
+												}
 													$sql->close();
 												
 										?>
